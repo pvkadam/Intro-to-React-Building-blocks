f511779a460b7a31c1aff60ef25ec0cd34529a40
pkadam <poojakadam8396@gmail.com>
2023-02-16 13:37:10 -0800

    Create react app skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2> Introduction to React Workshop</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
