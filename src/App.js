17cf2712536c2d0aba4aca3fcc214ae67307ce7a
Zachary Johnson <zjohnson@invoca.com>
2023-02-20 16:00:42 -0600

    Step: Move Input to component file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
 import Filters from './Filters';
+import Input from './Input';
 
 function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       {/* Input */}
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Input />
 
       {/* Filters for Challenge */}
       <Filters disabled={true}/>
