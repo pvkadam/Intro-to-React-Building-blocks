b9b55f13d362b9b6b104d547c0bf0de10b3666dc
Zachary Johnson <zjohnson@invoca.com>
2023-02-20 17:31:08 -0600

    Step: Add method to addItems and pass into Input component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ import { useState } from 'react';
 function App(props) {
   const [list, setList] = useState([{ label: 'Foo' }]);
 
+  const addItem = (item) => {
+    setList([...list, item]);
+  }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Input />
+      <Input addItem={addItem}/>
       <Filters disabled={true}/>
       <List listItems={list} addItems={setList} />
     </div>
