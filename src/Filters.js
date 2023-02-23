9ec0bf112466c9c20a31d8ec7b506b5373e542ec
Zachary Johnson <zjohnson@invoca.com>
2023-02-23 15:32:51 -0600

    Add filters file to main skeleton

---
+++ src/Filters.js
@@ -0,0 +1,24 @@
+
+export default function Filters(props) {
+  const { disabled } = props;
+
+  return (
+    <div className="filters btn-group stack-exception">
+      <button type="button" className="btn toggle-btn" aria-pressed="false" disabled={disabled}>
+        <span className="visually-hidden">Show </span>
+        <span>all</span>
+        <span className="visually-hidden"> tasks</span>
+      </button>
+      <button type="button" className="btn toggle-btn" aria-pressed="false" disabled={disabled}>
+        <span className="visually-hidden">Show </span>
+        <span>Active</span>
+        <span className="visually-hidden"> tasks</span>
+      </button>
+      <button type="button" className="btn toggle-btn" aria-pressed="false" disabled={disabled}>
+        <span className="visually-hidden">Show </span>
+        <span>Completed</span>
+        <span className="visually-hidden"> tasks</span>
+      </button>
+    </div>
+  )
+}
