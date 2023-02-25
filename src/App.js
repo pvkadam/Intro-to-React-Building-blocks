52c1b08339383c375799da8e19f365b0d9506e1f
Zachary Johnson <zjohnson@invoca.com>
2023-02-25 14:54:01 -0600

    Remove tasks remaining

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App(props) {
       <Filters disabled={true}/>
 
       {/* List */}
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">Tasks</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
